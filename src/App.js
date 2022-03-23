c087e1119b73f3d854b0b5ced6c23ca5708fe403
driss292 <drisskaci@outlook.fr>
2022-03-23 18:09:35 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
-    </div>
-  );
+  return <div>Hello from Beau Gosse</div>;
 }
 
 export default App;
